2d4bbdf0d6467e22c734ce9c1ea6ecd2768efe87
hocku <gajo216@gmail.com>
2023-09-08 23:24:16 +0300

    Push

--- js/main.js
+++ js/main.js
@@ -7,6 +7,88 @@ function checkElement(element) {
     }
 }
 
+async function checkLogin() {
+    response =  await fetch("http://localhost:8080/is_login", {
+        "method": "POST",
+        "mode": "cors",
+        "headers": {
+            "Content-Type": "application/json"
+        }
+    })
+    if (response.ok) {
+        let code = await response.json()
+        if (code == "400") {
+            return false
+        }
+
+        else if (code == "200") {
+            return true
+        }
+    }
+}
+
+
+function keyChange(object) {
+    let parent = document.getElementById("all_content")
+    let btn_log = document.getElementById("login_btn")
+
+    let elem = document.createElement("input")
+    elem.id = "login_btn"
+    elem.type = "submit"
+    elem.value = "Войти"
+
+    if (object.value) {
+        if (!btn_log) {
+            elem.addEventListener("click", event => {loginSend(event)})
+            parent.append(elem)
+        }
+    }
+    else {
+        btn_log.remove()
+    }
+}
+
+
+async function loginSend() {
+    let key
+    let target = event.target
+    let targetClassName = target.className
+    if (targetClassName == "key_value") {
+        key = target.innerText
+    }
+    else if (target.id == "login_btn") {
+        key = document.getElementById("key_obj").value
+    }
+
+    else {
+        return
+    }
+    let check = await checkLogin()
+    if (check) {
+        alert("Вход уже был выполнен")
+        window.location.href = "http://localhost:8080/main"
+    }
+    else {
+
+        let response = await fetch("http://localhost:8080/login", {
+            "method": "POST",
+            "mode": "cors",
+            "headers": {
+                "Content-Type": "application/json"
+            },
+            "body": JSON.stringify({"key": key})
+        })
+    
+    
+        if (response.status == 200) {
+            window.location.href = "http://localhost:8080/redir"
+            window.location.href = "http://localhost:8080/main"
+    
+        }
+    }
+}
+    
+
 async function checkRadio(object) {
     
     let exist_element = document.getElementById("key_obj")
@@ -23,7 +105,7 @@ async function checkRadio(object) {
 
 
     if (parent_name == "left_form_div") {
-        elem.innerHTML = '<input type="text" placeholder="VkKey" id="key_obj">'
+        elem.innerHTML = '<input type="text" placeholder="VkKey" id="key_obj" oninput="keyChange(this)">'
     }
 
     else if (parent_name == "right_form_div") {
